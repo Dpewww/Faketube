4e3d8f813456f70d6017285b3fd778df74b4afae
Dpewwww <dpew21@gmail.com>
2023-08-18 16:59:00 +0700

    add migration

--- database/migrations/2023_08_18_093629_create_channels_table.php
+++ database/migrations/2023_08_18_093629_create_channels_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('channels', function (Blueprint $table) {
-            $table->id;
+            $table->id();
             $table->string('Channel_id');
             $table->string('ChannelName');
             $table->string('Description');
